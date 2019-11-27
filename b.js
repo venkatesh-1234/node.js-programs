203d2dd29212ca61d54ccc14bedd630f6251b2df
venkatesh <venkatesh.chandu@securifi.com>
2019-11-27 10:08:42 +0530

    updated

---
+++ b.js
@@ -0,0 +1,6 @@
+var chandu = require("./a.js");
+var hi = require("./c.js");
+var venky = chandu.sum(10,20);
+var bye = hi.multiply(20,20);
+console.log("Value: "+venky);
+console.log("value: "+bye);
---
+++ sorting.js
@@ -0,0 +1,3 @@
+var venky=["hai","1ye","apple","2ango"];
+venky.sort();
+console.log(venky);
